dfcbd99a438305eb4e20c0956f50d79b1727c85a
kumarprince1995 <kumarprincesailor@rediffmail.com>
2021-08-30 20:16:34 +0530

    push

--- src/test/java/com/vtiger/products/assertion/java/Git2.java
+++ src/test/java/com/vtiger/products/assertion/java/Git2.java
@@ -1,5 +1,24 @@
 package com.vtiger.products.assertion.java;
 
+import org.testng.annotations.Test;
+
 public class Git2 {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute createOrg Test");
+ 	}	
+	
+	
+	@Test
+	public void createOrgWithIndustriesTest() {
+		System.out.println("execute createOrgWithIndustries Test");
+ 	}
+	
+	
+	@Test
+	public void createOrgTestWithRatingTest() {
+		System.out.println("execute createOrgTestWithRating Test");
+ 	}
 
 }
